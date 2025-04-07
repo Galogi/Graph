dfac06f79f7d37812b88ee674fd93f3a1ba8f8ee
Galogi <galogimaymon@gmail.com>
2025-04-07 23:38:16 +0300

    Update Kruskal.hpp

--- Graph/Algorithms/Kruskal.hpp
+++ Graph/Algorithms/Kruskal.hpp
@@ -1,9 +1,29 @@
 #pragma once
 #include "../Graph.hpp"
 
+/**
+ * @file Kruskal.hpp
+ * @brief Interface for Kruskal's algorithm to find a Minimum Spanning Tree (MST).
+ * @author galogimaymon@gmail.com
+ */
+
 namespace graph {
+
+    /**
+     * @class Kruskal
+     * @brief Provides a static method to run Kruskal's algorithm on a graph.
+     */
     class Kruskal {
     public:
+        /**
+         * @brief Runs Kruskal's algorithm to compute the Minimum Spanning Tree (MST).
+         * 
+         * @param g The input undirected, weighted, connected graph.
+         * @return Graph A new Graph object representing the MST.
+         * 
+         * @throws std::invalid_argument If the graph is not connected.
+         */
         static Graph run(const Graph& g);
     };
+
 }
