101a74cf1cb17a5cd39d29e59cae204375f5f890
Galogi <galogimaymon@gmail.com>
2025-04-07 23:45:06 +0300

    Update Graph.hpp

--- Graph/Graph.hpp
+++ Graph/Graph.hpp
@@ -1,47 +1,128 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+/**
+ * @file Graph.hpp
+ * @brief Defines the Node and Graph classes for representing an undirected weighted graph
+ *        using an adjacency list representation.
+ * @author galogimaymon@gmail.com
+ */
+
 namespace graph {
 
-struct Node {
-private:
-    const int val;       
-    const int weight;
-    int src;    
-    Node* next;
+    /**
+     * @struct Node
+     * @brief Represents a node in the adjacency list of a graph.
+     */
+    struct Node {
+    private:
+        const int val;       ///< The destination vertex.
+        const int weight;    ///< The weight of the edge.
+        int src;             ///< The source vertex.
+        Node* next;          ///< Pointer to the next node in the list.
 
-public:
-    Node(int val, int weight,int src ,Node* next = nullptr)
-        : val(val), weight(weight),src(src) ,next(next) {}
+    public:
+        /**
+         * @brief Constructs a new Node.
+         * @param val The destination vertex.
+         * @param weight The weight of the edge.
+         * @param src The source vertex.
+         * @param next Pointer to the next node (default: nullptr).
+         */
+        Node(int val, int weight, int src, Node* next = nullptr)
+            : val(val), weight(weight), src(src), next(next) {}
 
-    int getVal() const { return val; }
-    int getWeight() const { return weight; }
-    int getSrc() const { return src; }
+        int getVal() const { return val; }
+        int getWeight() const { return weight; }
+        int getSrc() const { return src; }
 
-    Node* getNext() const { return next; }
-    void setNext(Node* newNext) { next = newNext; }
-};
+        Node* getNext() const { return next; }
+        void setNext(Node* newNext) { next = newNext; }
+    };
 
-class Graph {
-    const int N;
-    Node** head;
+    /**
+     * @class Graph
+     * @brief Represents an undirected weighted graph using an adjacency list.
+     */
+    class Graph {
+        const int N;         ///< Number of vertices.
+        Node** head;         ///< Array of adjacency list heads.
 
-    Node* getAdjListNode(int dest, int weight, int src , Node* head);
+        /**
+         * @brief Creates a new adjacency list node.
+         * @param dest The destination vertex.
+         * @param weight The weight of the edge.
+         * @param src The source vertex.
+         * @param head The current head of the list.
+         * @return Pointer to the new Node.
+         */
+        Node* getAdjListNode(int dest, int weight, int src, Node* head);
 
-public:
-    Graph(int N);
-    ~Graph();
+    public:
+        /**
+         * @brief Constructs a new Graph with a given number of vertices.
+         * @param N The number of vertices.
+         */
+        Graph(int N);
 
-    void addEdge(int src, int dest, int weight = 1);
-    void removeEdge(int src, int dest);
-    void print_graph();
-    void print_Tree();
-    Node* getHead(int v) const;
-    int getSize() const;
-    Graph(const Graph& other);                // copy constructor
-    Graph& operator=(const Graph& other);
-};
+        /**
+         * @brief Destructor that frees allocated memory.
+         */
+        ~Graph();
+
+        /**
+         * @brief Adds an undirected edge between two vertices.
+         * @param src Source vertex.
+         * @param dest Destination vertex.
+         * @param weight Weight of the edge (default: 1).
+         */
+        void addEdge(int src, int dest, int weight = 1);
+
+        /**
+         * @brief Removes the edge between two vertices (both directions).
+         * @param src Source vertex.
+         * @param dest Destination vertex.
+         */
+        void removeEdge(int src, int dest);
+
+        /**
+         * @brief Prints the entire adjacency list of the graph.
+         */
+        void print_graph();
+
+        /**
+         * @brief Prints the tree representation (for traversal results).
+         */
+        void print_Tree();
+
+        /**
+         * @brief Returns the head of the adjacency list for a given vertex.
+         * @param v The vertex index.
+         * @return Pointer to the head node.
+         */
+        Node* getHead(int v) const;
+
+        /**
+         * @brief Returns the number of vertices in the graph.
+         * @return Number of vertices.
+         */
+        int getSize() const;
+
+        /**
+         * @brief Copy constructor.
+         * @param other The graph to copy.
+         */
+        Graph(const Graph& other);
+
+        /**
+         * @brief Copy assignment operator.
+         * @param other The graph to assign.
+         * @return Reference to this graph.
+         */
+        Graph& operator=(const Graph& other);
+    };
 
 }
 
 #endif
+
