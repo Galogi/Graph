435a4fd912ac603df7fa4226ec179f2d71dbf7fd
Galogi <galogimaymon@gmail.com>
2025-04-07 23:40:41 +0300

    Update AlgorithmType.hpp

--- Graph/AlgorithmType.hpp
+++ Graph/AlgorithmType.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
+/**
+ * @file AlgorithmType.hpp
+ * @brief Defines an enumeration for selecting graph algorithm types at runtime or via interface logic.
+ * @author galogimaymon@gmail.com
+ */
+
 namespace graph {
+
+    /**
+     * @enum AlgorithmType
+     * @brief Enumerates supported graph algorithms.
+     */
     enum class AlgorithmType {
-        BFS,
-        DFS,
-        Dijkstra,
-        Prim,
-        Kruskal
+        BFS,       ///< Breadth-First Search
+        DFS,       ///< Depth-First Search
+        Dijkstra,  ///< Dijkstra's Shortest Path Algorithm
+        Prim,      ///< Prim's Minimum Spanning Tree Algorithm
+        Kruskal    ///< Kruskal's Minimum Spanning Tree Algorithm
     };
+
 }
